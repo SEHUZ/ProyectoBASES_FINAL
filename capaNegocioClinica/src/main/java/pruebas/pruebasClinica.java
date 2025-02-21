d9fdffe657eedf64ea767b7e4b3781b6742173f1
DanielMR0509 <daniel.miramontes252801@potros.itson.edu.mx>
2025-02-20 17:44:43 -0700

    Agregada clase de pruebas

---
+++ capaNegocioClinica/src/main/java/pruebas/pruebasClinica.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebas;
+
+import Exception.NegocioException;
+
+/**
+ *
+ * @author Daniel M
+ */
+public class pruebasClinica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try {
+            
+        } catch (NegocioException e) {
+            
+        }
+    }
+    
+}
